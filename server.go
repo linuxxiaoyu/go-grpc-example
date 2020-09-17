e1f299d8ef4e55c4e5caab00f13f0565fe3876b9
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-09-17 09:53:41 +0800

    Add server

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/linuxxiaoyu/go-grpc-example/proto"
+	"google.golang.org/grpc"
+)
+
+type SearchService struct{}
+
+func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+	return &pb.SearchResponse{Response: r.GetRequest() + "Server"}, nil
+}
+
+const PORT = "9001"
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterSearchServiceServer(server, &SearchService{})
+
+	lis, err := net.Listen("tcp", ":"+PORT)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+
+	server.Serve(lis)
+}
