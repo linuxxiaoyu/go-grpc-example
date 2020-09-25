c7ebb809a8d5975cfaebdd348b5a8fe50d0f5724
linuxxiaoyu <linuxxiaoyu@gmail.com>
2020-09-25 16:48:47 +0800

    Add math.proto

--- client/simple_client/client.go
+++ client/simple_client/client.go
@@ -2,28 +2,54 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 
 	pb "github.com/linuxxiaoyu/go-grpc-example/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 const PORT = "9001"
 
 func main() {
-	conn, err := grpc.Dial(":"+PORT, grpc.WithInsecure())
+	cert, err := tls.LoadX509KeyPair("../../conf/client/client.pem", "../../conf/client/client.key")
+	if err != nil {
+		log.Fatalf("tls.LoadX509KeyPair err: %v", err)
+	}
+
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("../../conf/ca.pem")
+	if err != nil {
+		log.Fatalf("iotuil.ReadFile err: %v", err)
+	}
+
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("cartPool.AppendCertsFromPEM err")
+	}
+
+	c := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ServerName:   "go-grpc-example",
+		RootCAs:      certPool,
+	})
+
+	conn, err := grpc.Dial(":"+PORT, grpc.WithTransportCredentials(c))
 	if err != nil {
 		log.Fatalf("grpc.Dial err: %v", err)
 	}
 	defer conn.Close()
 
-	client := pb.NewSearchServiceClient(conn)
-	resp, err := client.Search(context.Background(), &pb.SearchRequest{
-		Request: "gRPC",
+	client := pb.NewMathServiceClient(conn)
+	resp, err := client.Div(context.Background(), &pb.MathRequest{
+		A: 10,
+		B: 3,
 	})
 	if err != nil {
 		log.Fatalf("client.Search err: %v", err)
 	}
 
-	log.Printf("resp: %s", resp.GetResponse())
+	log.Printf("resp: %d", resp.GetRet())
 }
--- server/simple_server/server.go
+++ server/simple_server/server.go
@@ -2,24 +2,74 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
+	"errors"
+	"io/ioutil"
 	"log"
 	"net"
 
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	pb "github.com/linuxxiaoyu/go-grpc-example/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-type SearchService struct{}
+// type SearchService struct{}
 
-func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
-	return &pb.SearchResponse{Response: r.GetRequest() + "Server"}, nil
+// func (s *SearchService) Search(ctx context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+// 	return &pb.SearchResponse{Response: r.GetRequest() + " Server"}, nil
+// }
+
+type MathService struct{}
+
+func (s *MathService) Div(ctx context.Context, r *pb.MathRequest) (*pb.MathResponse, error) {
+	return &pb.MathResponse{Ret: r.A / r.B}, nil
 }
 
 const PORT = "9001"
 
+func GetTLSCredentialsByCA() (credentials.TransportCredentials, error) {
+	cert, err := tls.LoadX509KeyPair("../../conf/server/server.pem", "../../conf/server/server.key")
+	if err != nil {
+		return nil, err
+	}
+
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile("../../conf/ca.pem")
+	if err != nil {
+		return nil, err
+	}
+
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		return nil, errors.New("certPool.AppendCertsFromPEM err")
+	}
+
+	c := credentials.NewTLS(&tls.Config{
+		Certificates: []tls.Certificate{cert},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+	})
+
+	return c, nil
+}
+
 func main() {
-	server := grpc.NewServer()
-	pb.RegisterSearchServiceServer(server, &SearchService{})
+	c, err := GetTLSCredentialsByCA()
+	if err != nil {
+		log.Fatalf("GetTLSCredentialsByCA err: %v", err)
+	}
+
+	opts := []grpc.ServerOption{
+		grpc.Creds(c),
+		grpc_middleware.WithUnaryServerChain(
+			LoggingInterceptor,
+			RecoveryInterceptor,
+		),
+	}
+
+	server := grpc.NewServer(opts...)
+	pb.RegisterMathServiceServer(server, &MathService{})
 
 	lis, err := net.Listen("tcp", ":"+PORT)
 	if err != nil {
